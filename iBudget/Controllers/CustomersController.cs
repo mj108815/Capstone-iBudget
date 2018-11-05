8c78871bd37b0c24916bcd2ac51c51074fc90a42
mj108815 <mj108815@yahoo.com>
2018-11-05 13:43:40 -0600

    started to work on the sendgrid api in the customer controller and uncommented out the subscribed check box back

--- iBudget/Controllers/CustomersController.cs
+++ iBudget/Controllers/CustomersController.cs
@@ -12,6 +12,10 @@ using System.Security.Claims;
 using RestSharp;
 using RestSharp.Authenticators;
 using System.Net;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Text.RegularExpressions;
+using System.Net.Mail;
 
 namespace iBudget.Controllers
 {
@@ -29,7 +33,6 @@ namespace iBudget.Controllers
         // GET: Customers
         public async Task<IActionResult> Index()
         {
-            //var applicationDbContext = _context.Customers.Include(c => c.ApplicationUser);
             return View(await _context.Customers.ToListAsync());
         }
         public async Task<IActionResult> FinancialAnalystIndex()
@@ -40,14 +43,6 @@ namespace iBudget.Controllers
         // GET: Customers/Details/5
         public async Task<IActionResult> Details(int? id)
         {
-            //if (id == null)
-            //{
-            //    return NotFound();
-            //}
-
-            //var customer = await _context.Customers
-            //    .Include(c => c.ApplicationUser)
-            //    .FirstOrDefaultAsync(m => m.CustomerID == id);
             var userId = _userManager.GetUserId(HttpContext.User);
             var user = await _context.Customers
                 .FirstOrDefaultAsync(m => m.ApplicationUserId == userId);
@@ -56,12 +51,6 @@ namespace iBudget.Controllers
                 return NotFound();
             }
             return View(user);
-            //if (customer == null)
-            //{
-            //    return NotFound();
-            //}
-
-            //return View(customer);
         }
         public IActionResult FinancialAnalystDetails(int? id)
         {
@@ -94,7 +83,24 @@ namespace iBudget.Controllers
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            SendSimpleMessage(); // will go in the calendar when working
+            //var mailMessage = new MailMessage
+            //{
+            //    From = new MailAddress("support@dotnetthoughts.net"),
+            //    Subject = "Hello World",
+            //    Body = "Test email from Send Grid SMTP Settings"
+            //};
+
+            //mailMessage.To.Add("anuraj@dotnetthoughts.net");
+
+            //var smtpClient = new SmtpClient
+            //{
+            //    Credentials = new NetworkCredential("Your-Username@azure.com", "Your-Password"),
+            //    Host = "smtp.sendgrid.net",
+            //    Port = 587
+            //};
+
+            //smtpClient.Send(mailMessage);
+            await SendEmail();
             ViewData["ApplicationUserId"] = new SelectList(_context.ApplicationUsers, "Id", "Id", customer.ApplicationUserId);
             return View(customer);
         }
@@ -102,18 +108,6 @@ namespace iBudget.Controllers
         // GET: Customers/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
-            //if (id == null)
-            //{
-            //    return NotFound();
-            //}
-
-            //var customer = await _context.Customers.FindAsync(id);
-            //if (customer == null)
-            //{
-            //    return NotFound();
-            //}
-            //ViewData["ApplicationUserId"] = new SelectList(_context.ApplicationUsers, "Id", "Id", customer.ApplicationUserId);
-            //return View(customer);
             var userId = _userManager.GetUserId(HttpContext.User);
             var user = await _context.Customers
                 .FirstOrDefaultAsync(m => m.ApplicationUserId == userId);
@@ -213,24 +207,24 @@ namespace iBudget.Controllers
                 return View(customer);
             }
         }
-        public static IRestResponse SendSimpleMessage() 
-        {
-            RestClient client = new RestClient();
-            client.BaseUrl = new Uri("https://api.mailgun.net/v3");
-            client.Authenticator =
-                new HttpBasicAuthenticator("api",
-                                            Key.mailgunKey);
-            RestRequest request = new RestRequest();
-            request.AddParameter("domain", "sandboxd38a61ad30c840cfb92cf1515f62e7ea.mailgun.org", ParameterType.UrlSegment);
-            request.Resource = "{domain}/messages";
-            request.AddParameter("from", "Mailgun Sandbox <mailgun@sandboxd38a61ad30c840cfb92cf1515f62e7ea.mailgun.org>");
-            request.AddParameter("to", "marisajanowski@gmail.com");
-            request.AddParameter("to", "YOU@sandboxd38a61ad30c840cfb92cf1515f62e7ea.mailgun.org");
-            request.AddParameter("subject", "Hello");
-            request.AddParameter("text", "Testing some Mailgun awesomeness!");
-            request.Method = Method.POST;
-            return client.Execute(request);
-        }
+        //public static IRestResponse SendSimpleMessage() 
+        //{
+        //    RestClient client = new RestClient();
+        //    client.BaseUrl = new Uri("https://api.mailgun.net/v3");
+        //    client.Authenticator =
+        //        new HttpBasicAuthenticator("api",
+        //                                    Key.mailgunKey);
+        //    RestRequest request = new RestRequest();
+        //    request.AddParameter("domain", "sandboxd38a61ad30c840cfb92cf1515f62e7ea.mailgun.org", ParameterType.UrlSegment);
+        //    request.Resource = "{domain}/messages";
+        //    request.AddParameter("from", "Mailgun Sandbox <mailgun@sandboxd38a61ad30c840cfb92cf1515f62e7ea.mailgun.org>");
+        //    request.AddParameter("to", "marisajanowski@gmail.com");
+        //    request.AddParameter("to", "YOU@sandboxd38a61ad30c840cfb92cf1515f62e7ea.mailgun.org");
+        //    request.AddParameter("subject", "Hello");
+        //    request.AddParameter("text", "Testing some Mailgun awesomeness!");
+        //    request.Method = Method.POST;
+        //    return client.Execute(request);
+        //}
         //private async Task SendEmail(string email, string subject, string htmlContent)
         //{
         //    var apiKey = "YOUR SENDGRID API Key";
@@ -241,5 +235,17 @@ namespace iBudget.Controllers
         //    var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
         //    var response = await client.SendEmailAsync(msg);
         //}
+        public static async Task SendEmail()
+        {
+            var apiKey = Environment.GetEnvironmentVariable("NAME_OF_THE_ENVIRONMENT_VARIABLE_FOR_YOUR_SENDGRID_KEY");
+            var client = new SendGridClient(Key.sendGridKey);
+            var from = new EmailAddress("mj108815@gmail.com", "Example User");
+            var subject = "Sending with SendGrid is Fun";
+            var to = new EmailAddress("mj108815@gmail.com", "Example User");
+            var plainTextContent = "and easy to do anywhere, even with C#";
+            var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
+        }
     }
 }
